32b72650ff4a6abc20a1c3699f3c077830736502
TMKha <tranminhkha12cb4tqt@gmail.com>
2023-09-13 22:45:47 +0700

    update fix cors

--- backend/index.js
+++ backend/index.js
@@ -11,15 +11,15 @@ app.use(express.json());
 
 //Middleware for handling CORS POLICY
 // Option 1: Allow all origins
-// app.use(cors());
+app.use(cors());
 // Option 2: Allow custom origins
-app.use(
-    cors({
-        origin:'https://bookstore-api-pi.vercel.app',
-        methods:['GET','POST','PUT','DELETE'],
-        allowedHeaders:['Content-Type']
-    })
-)
+// app.use(
+//     cors({
+//         origin:'https://bookstore-api-pi.vercel.app',
+//         methods:['GET','POST','PUT','DELETE'],
+//         allowedHeaders:['Content-Type']
+//     })
+// )
 
 app.get("/",(request,respone)=>{
     console.log(request);
