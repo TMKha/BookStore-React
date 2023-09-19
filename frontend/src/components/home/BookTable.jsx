9af976783e49474429799f79ccf250104cbc59b6
TMKha <tranminhkha12cb4tqt@gmail.com>
2023-09-19 11:11:33 +0700

    update router for table

--- frontend/src/components/home/BookTable.jsx
+++ frontend/src/components/home/BookTable.jsx
@@ -37,13 +37,13 @@ const BookTable = ({books}) => {
                      </td>
                      <td className="border border-slate-700 rounded-md text-center ">
                        <div className="flex justify-center gap-x-4">
-                         <Link to={`/books/details/${book._id}`}>
+                         <Link to={`/books/${book._id}`}>
                            <BsInfoCircle className="text-green-800 text-2xl" />
                          </Link>
-                         <Link to={`/books/edit/${book._id}`}>
+                         <Link to={`/books/${book._id}`}>
                            <AiOutlineEdit className="text-yellow-800 text-2xl" />
                          </Link>
-                         <Link to={`/books/delete/${book._id}`}>
+                         <Link to={`/books/${book._id}`}>
                            <MdOutlineDelete className="text-red-800 text-2xl" />
                          </Link>
                        </div>
